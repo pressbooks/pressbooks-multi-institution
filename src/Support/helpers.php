09d8ef689f9d896194d1530ec9a8812f76a2d5e1
Oscar Arzola <oscararzola@gmail.com>
2024-02-29 11:48:42 -0500

    chore: remove dead code

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -6,8 +6,8 @@ use PressbooksMultiInstitution\Models\InstitutionUser;
 
 use function Pressbooks\Admin\NetworkManagers\is_restricted;
 
-function get_institution_by_manager($user = null): int
+function get_institution_by_manager(): int
 {
-    $user = ($user instanceof \WP_User && $user->ID !== 0) ? $user : wp_get_current_user();
+    $user = wp_get_current_user();
     return is_restricted() ? InstitutionUser::query()->isManager($user->ID)->first()?->institution_id ?? 0 : 0;
 }
