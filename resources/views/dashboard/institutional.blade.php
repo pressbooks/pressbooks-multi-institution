2447980ad8d6b9af3efce9a4d50fd336bb10502a
Ho Man Chan <homan98@gmail.com>
2024-03-06 10:50:26 -0500

    fix: update network manager dashboard to reuse support resources code (#79)
    * fix: update support resources in dashboard for institutional managers
    
    * fix: correct ids from support resources
    
    * fix: typo
    
    * fix: linting
    
    * chore: add include support resources in IM dashboard
    
    * fix: uncomment autoloader for testing
    
    * chore: update pressbooks/pressbooks dependency for reusing blade file
    
    ---------
    
    Co-authored-by: Steel Wagstaff <steel@pressbooks.com>

--- resources/views/dashboard/institutional.blade.php
+++ resources/views/dashboard/institutional.blade.php
@@ -60,54 +60,5 @@
 				</div>
 			</div>
 		</div>
-	<div class="pb-dashboard-row">
-		<div class="pb-dashboard-panel">
-			<div class="pb-dashboard-content">
-				<h2>{{ __('Support resources', 'pressbooks-multi-institution') }}</h2>
-				{{-- TODO: add link to new YouTube playlist. --}}
-				<ul class="horizontal">
-					<li class="resources" id="getting-started">
-						<a href="https://youtube.com/playlist?list=PLMFmJu3NJhevTbp5XAbdif8OloNhqOw5n" target="_blank">
-							<img
-								src="{{ PB_PLUGIN_URL . "assets/dist/images/pb-getting-started.png" }}"
-								alt=""
-							/>
-							{{ __('Getting started with Pressbooks', 'pressbooks-multi-institution' )}}
-						</a>
-						<p>{{ __( 'Watch a short video series on how to get started with Pressbooks.', 'pressbooks-multi-institution' ) }}</p>
-					</li>
-					<li class="resources" id="pressbooks-guide">
-						<a href="https://guide.pressbooks.com" target="_blank">
-							<img
-								src="{{ PB_PLUGIN_URL . "assets/dist/images/pb-guide.png" }}"
-								alt=""
-							/>
-							{{ __('Pressbooks user guide', 'pressbooks-multi-institution' )}}
-						</a>
-						<p>{{ __( 'Find help and how-tos for your publishing project in this detailed handbook.', 'pressbooks-multi-institution' ) }}</p>
-					</li>
-					<li class="resources" id="forum">
-						<a href="https://pressbooks.community" target="_blank">
-							<img
-								src="{{ PB_PLUGIN_URL . "assets/dist/images/pb-forum.png" }}"
-								alt=""
-							/>
-							{{ __('Pressbooks community forum', 'pressbooks-multi-institution' ) }}
-						</a>
-						<p>{{ __( 'Discuss Pressbooks related questions with other users in our public forum.', 'pressbooks-multi-institution' ) }}</p>
-					</li>
-					<li class="resources" id="webinars">
-						<a href="https://pressbooks.com/webinars" target="_blank">
-							<img
-								src="{{ PB_PLUGIN_URL . "assets/dist/images/pb-webinars.png" }}"
-								alt=""
-							/>
-							{{ __('Pressbooks training webinars', 'pressbooks-multi-institution') }}
-						</a>
-						<p>{{ __( 'Register for free webinars to learn about Pressbooks features and best practices.', 'pressbooks-multi-institution' ) }}</p>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+	@include('admin.dashboard.support')
 </div>
