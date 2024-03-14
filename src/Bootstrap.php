9425fafc44dfc5ae5e6691510769b4d87c0d5d8f
Ricardo Aragon <richard015ar@gmail.com>
2024-03-14 07:49:45 -0300

    fix: add menu items only for main site

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -104,7 +104,9 @@ final class Bootstrap
     private function registerActions(): void
     {
         add_action('network_admin_menu', [$this, 'registerMenus'], 11);
-        add_action('admin_menu', [$this, 'registerMenus'], 11);
+		if ( is_main_site() ) {
+			add_action('admin_menu', [$this, 'registerMenus'], 11);
+		}
         add_action('user_register', fn (int $id) => app(AssignUserToInstitution::class)->handle($id));
         add_action('pb_new_blog', fn () => app(AssignBookToInstitution::class)->handle());
         add_action('network_admin_menu', fn () => app(PermissionsManager::class)->handleMenus(), 1000);
