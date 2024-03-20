d3b6617d5fc107c92f19706298399422c2dc577b
SteelWagstaff <steel@pressbooks.com>
2024-03-20 12:51:05 -0700

    fix: improve validation error message

--- resources/views/partials/errors/invalid-domains.blade.php
+++ resources/views/partials/errors/invalid-domains.blade.php
@@ -1,11 +1,14 @@
 <li>
-	<span>{{ _n('Email domains can only contain alphanumeric characters, periods, and dashes. The following entry contained invalid content:', 'Email domains can only contain alphanumeric characters, periods, and dashes. The following entries contained invalid content:', count($domains), 'pressbooks-multi-institution') }}</span>
-	<span class="indented">
+	<p>{{ __('Valid email domains can only include letters (a-z), numbers, periods, and dashes. They must include at least one period, which cannot come at the beginning or end of the domain. A period or dash must be followed by one or more letter or number.', 'pressbooks_multi_institution' )}}</p>
+	<p>
+		{{ _n('The following entry contained invalid content:', 'The following entries contained invalid content:', count($domains), 'pressbooks-multi-institution') }}
+	</p>
+		<ul class="invalid">
 		@foreach($domains as $domain)
-			<strong>{{ $domain }}</strong>
+			<li><strong>{{ $domain }}</strong></li>
 		@endforeach
-	</span>
-	<span class="indented">
+		</ul>
+	<p>
 		{{ __('Please correct the invalid content and resubmit the form.', 'pressbooks-multi-institution') }}
-	</span>
+	</p>
 </li>
