02863b6789ada9eec57295d7c8c8eff251c3fd04
SteelWagstaff <steel@pressbooks.com>
2024-03-20 13:14:20 -0700

    fix: linting

--- resources/views/partials/errors/invalid-domains.blade.php
+++ resources/views/partials/errors/invalid-domains.blade.php
@@ -1,5 +1,7 @@
 <li>
-	<p>{{ __('Valid email domains can only include letters (a-z), numbers, periods, and dashes. They must include at least one period, which cannot come at the beginning or end of the domain. A period or dash must be followed by one or more letter or number.', 'pressbooks_multi_institution' )}}</p>
+	<p>
+		{{ __('Valid email domains can only include letters (a-z), numbers, periods, and dashes. They must include at least one period, which cannot come at the beginning or end of the domain. A period or dash must be followed by one or more letter or number.', 'pressbooks_multi_institution' )}}
+	</p>
 	<p>
 		{{ _n('The following entry contained invalid content:', 'The following entries contained invalid content:', count($domains), 'pressbooks-multi-institution') }}
 	</p>
