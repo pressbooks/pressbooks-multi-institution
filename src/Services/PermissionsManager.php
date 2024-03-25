5f2137f4095f771e8e3fba9e62d9a09fed0a40ff
Oscar Arzola <oscararzola@gmail.com>
2024-03-25 17:17:42 -0400

    chore: merge dev

--- src/Services/PermissionsManager.php
+++ src/Services/PermissionsManager.php
@@ -178,4 +178,39 @@ class PermissionsManager
             }
         });
     }
+
+    /**
+     * This method is called after an institution is updated or deleted, and it updates the restricted users list
+     *
+     * @param array<int, int> $newManagers
+     * @param array<int, int> $revokedManagers
+     */
+    public static function syncRestrictedUsers(array $newManagers, array $revokedManagers): void
+    {
+        $restricted = _restricted_users();
+
+        // Grant super admin privileges to new institution managers and add them to the restricted users list
+        foreach ($newManagers as $id) {
+            $restricted[] = $id;
+
+            grant_super_admin($id);
+        }
+
+        $restricted = array_diff(array_unique($restricted), $revokedManagers);
+
+        // Remove institution managers from the restricted users list and revoke their super admin privileges
+        foreach ($revokedManagers as $id) {
+            revoke_super_admin($id);
+        }
+
+        // Update the restricted users list
+        update_site_option('pressbooks_network_managers', $restricted);
+    }
+
+    public static function revokeInstitutionalManagersPrivileges(): void
+    {
+        $managerIds = InstitutionUser::query()->managers()->pluck('user_id')->toArray();
+
+        self::syncRestrictedUsers([], $managerIds);
+    }
 }
