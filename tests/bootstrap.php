2918e224198a877dccee6480ab07efc0562c9ad8
Ned Zimmerman <ned@bight.dev>
2024-03-31 10:41:32 -0300

    fix: format files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,8 @@ $tests_dir = env('WP_TESTS_DIR', '/tmp/wordpress-tests-lib');
 require_once "{$tests_dir}/includes/functions.php";
 
 tests_add_filter('muplugins_loaded', function () {
-    require_once( __DIR__ . '/../../pressbooks/pressbooks.php' );
-    require_once( __DIR__ . '/../../pressbooks/tests/utils-trait.php' );
+    require_once(__DIR__ . '/../../pressbooks/pressbooks.php');
+    require_once(__DIR__ . '/../../pressbooks/tests/utils-trait.php');
 });
 
 require_once "{$tests_dir}/includes/bootstrap.php";
