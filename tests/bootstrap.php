cf9059747441aafc1386d883e722f42471a44000
Ned Zimmerman <ned@bight.dev>
2024-03-31 10:51:55 -0300

    test: fix test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,9 @@
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+$tests_dir = getenv('WP_TESTS_DIR');
+if (! $tests_dir) {
+    $tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 require_once "{$tests_dir}/includes/functions.php";
