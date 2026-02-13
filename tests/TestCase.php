5e1f26e77e555f97332705959f28a4c2996d60f4
Steel Wagstaff <steel@pressbooks.com>
2026-02-12 20:49:04 -0800

    fix: improve teardown process by reordering deletion of tables

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,10 +23,14 @@ class TestCase extends WP_UnitTestCase
         /** @var Manager $db */
         $db = app('db');
 
+        // Delete tables with foreign key constraints first
+        $db->table('institution_users')->delete();
+        $db->table('institutions_blogs')->delete();
+
+        // Then delete the referenced tables
+        $db->table('institutions')->delete();
         $db->table('users')->where('user_login', '<>', 'admin')->delete();
         $db->table('blogs')->where('blog_id', '<>', get_main_site_id())->delete();
-        $db->table('institutions')->delete();
-        $db->table('institution_users')->delete();
 
         Migration::rollback();
     }
