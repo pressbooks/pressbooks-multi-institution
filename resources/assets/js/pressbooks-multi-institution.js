908f34328c65dcfa166066397b8f8437a35be06a
Oscar Arzola <oscararzola@gmail.com>
2026-03-11 14:01:23 -0400

    feat: new build tools (#291)
    * feat: new build tools
    
    * update deps
    
    * build

--- resources/assets/js/pressbooks-multi-institution.js
+++ resources/assets/js/pressbooks-multi-institution.js
@@ -1,17 +1,17 @@
 /* global context */
-import "../styles/pressbooks-multi-institutions.css";
+import '../styles/pressbooks-multi-institutions.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-	const doAction = document.querySelector(`${context.formSelector}  #doaction`);
+document.addEventListener( 'DOMContentLoaded', function () {
+	const doAction = document.querySelector( `${ context.formSelector }  #doaction` );
 
-	doAction && doAction.addEventListener('click', function (e) {
+	doAction && doAction.addEventListener( 'click', function ( e ) {
 		e.preventDefault();
 
-		const action = document.querySelector(`${context.formSelector} #bulk-action-selector-top`).value;
-		const items = document.querySelectorAll(`${context.formSelector} .check-column input:checked`);
+		const action = document.querySelector( `${ context.formSelector } #bulk-action-selector-top` ).value;
+		const items = document.querySelectorAll( `${ context.formSelector } .check-column input:checked` );
 
-		if (action !== '-1' && items.length > 0 && confirm(context.confirmationMessage)) {
-			document.querySelector(context.formSelector).submit();
+		if ( action !== '-1' && items.length > 0 && confirm( context.confirmationMessage ) ) {
+			document.querySelector( context.formSelector ).submit();
 		}
-	});
-});
+	} );
+} );
